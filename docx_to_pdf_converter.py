db33903fe2c7259876f5482c20b71efb4875d0d8
hafeeznathoo <nathoohafeez@gmail.com>
2024-02-27 07:37:09 -0700

    clarified variable names

--- docx_to_pdf_converter.py
+++ docx_to_pdf_converter.py
@@ -12,7 +12,7 @@ destination = r".\test_destination"
 def convert_file(filenameInput, filenameOutput):
     docx2pdf.convert(filenameInput, filenameOutput)
 
-#obtain list of all OSCE checklist paths and names in dict
+#obtain list of all doc paths and names in dict
 targets = {}
 for docfile in p.Path(src).glob("**/*"):
     print(docfile)
@@ -25,6 +25,6 @@ for docfile in p.Path(src).glob("**/*"):
 #do the conversion
 for name in targets:
     old_path = targets[name]
-    course_name = old_path.parent.name
-    new_path = destination + '\\' +course_name +'_' + name + ".pdf"
+    parent_name = old_path.parent.name
+    new_path = destination + '\\' +parent_name +'_' + name + ".pdf"
     convert_file(old_path, new_path)
